4b6f65c14fddec449f180228c689f8a52de2faa6
Tom Millichamp <tom@edgewords.co.uk>
2019-01-17 09:09:10 +0000

    pickles as msbuild job

---
+++ com.edgewords.mandm/PicklesDocs/pickledFeatures.js
@@ -0,0 +1,147 @@
+jsonPWrapper ({
+  "Features": [
+    {
+      "RelativeFolder": "Basket.feature",
+      "Feature": {
+        "Name": "Basket",
+        "Description": "As a customer I want to add items to the basket to purchase later",
+        "FeatureElements": [
+          {
+            "Name": "Add Item to Basket",
+            "Slug": "add-item-to-basket",
+            "Description": "",
+            "Steps": [
+              {
+                "Keyword": "Given",
+                "NativeKeyword": "Given ",
+                "Name": "that I am on the home page",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "When",
+                "NativeKeyword": "When ",
+                "Name": "I add an item to the basket",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "Then",
+                "NativeKeyword": "Then ",
+                "Name": "it is displayed in the basket",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              }
+            ],
+            "Tags": [
+              "@functional"
+            ],
+            "Result": {
+              "WasExecuted": false,
+              "WasSuccessful": false,
+              "WasProvided": false
+            }
+          },
+          {
+            "Name": "Remove Item from Basket",
+            "Slug": "remove-item-from-basket",
+            "Description": "",
+            "Steps": [
+              {
+                "Keyword": "Given",
+                "NativeKeyword": "Given ",
+                "Name": "that I am on the home page",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "When",
+                "NativeKeyword": "When ",
+                "Name": "I add an item to the basket",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "And",
+                "NativeKeyword": "And ",
+                "Name": "I then remove it from the basket",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "Then",
+                "NativeKeyword": "Then ",
+                "Name": "the basket is empty",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              }
+            ],
+            "Tags": [
+              "@functional"
+            ],
+            "Result": {
+              "WasExecuted": false,
+              "WasSuccessful": false,
+              "WasProvided": false
+            }
+          }
+        ],
+        "Result": {
+          "WasExecuted": false,
+          "WasSuccessful": false,
+          "WasProvided": false
+        },
+        "Tags": []
+      },
+      "Result": {
+        "WasExecuted": false,
+        "WasSuccessful": false,
+        "WasProvided": false
+      }
+    }
+  ],
+  "Summary": {
+    "Tags": [
+      {
+        "Tag": "@functional",
+        "Total": 2,
+        "Passing": 0,
+        "Failing": 0,
+        "Inconclusive": 2
+      }
+    ],
+    "Folders": [
+      {
+        "Folder": "Basket.feature",
+        "Total": 2,
+        "Passing": 0,
+        "Failing": 0,
+        "Inconclusive": 2
+      }
+    ],
+    "NotTestedFolders": [
+      {
+        "Folder": "Basket.feature",
+        "Total": 0,
+        "Passing": 0,
+        "Failing": 0,
+        "Inconclusive": 0
+      }
+    ],
+    "Scenarios": {
+      "Total": 2,
+      "Passing": 0,
+      "Failing": 0,
+      "Inconclusive": 2
+    },
+    "Features": {
+      "Total": 1,
+      "Passing": 0,
+      "Failing": 0,
+      "Inconclusive": 1
+    }
+  },
+  "Configuration": {
+    "GeneratedOn": "17 January 2019 08:21:54"
+  }
+});
